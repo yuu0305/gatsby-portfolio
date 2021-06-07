591117815e1350bd75bc79b740bcffe7d13c70d4
yuu0305 <segyayuu8@gmail.com>
2021-06-07 23:40:33 +0900

    Display image on eåach blog
    Display image on eåach blog

--- src/templates/single-blog.js
+++ src/templates/single-blog.js
@@ -1,13 +1,19 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const SingleBlog = (props) =>{
     return (
+        <>
+        <div>
+            <GatsbyImage image={props.data.markdownRemark.frontmatter.image.childImageSharp.gatsbyImageData} alt="blog-image"/>
+        </div>
         <div>
           <h1>{props.data.markdownRemark.frontmatter.title}</h1>
           <p>{props.data.markdownRemark.frontmatter.date}</p>
           <div dangerouslySetInnerHTML={{__html:props.data.markdownRemark.html}} />
         </div>
+        </>
     )
 }
 
@@ -19,6 +25,16 @@ export const query = graphql`
         date
         excerpt
         id
+        image {
+            childImageSharp {
+              gatsbyImageData(
+                quality: 90,
+                placeholder: BLURRED,
+                formats: AUTO,
+                width: 1000
+              )
+            }
+          }
         title
     }
     html
