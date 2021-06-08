be6c821e97a3d19557f269aa0a183562d42e3d65
yuu0305 <segyayuu8@gmail.com>
2021-06-08 21:24:05 +0900

    Add header  footer component
    Add header  footer component to import header  & footer

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Header from "./header"
+import  Footer from "./footer"
+
+const Layout = () => {
+    return(
+        <>
+        <Header />
+        <Footer />
+        </>
+    )
+}
