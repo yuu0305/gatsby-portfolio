617c9d42c6ec5f09224211916b5f7c7510ac0939
yuu0305 <segyayuu8@gmail.com>
2021-06-08 20:53:54 +0900

    Create Contact page
    Create Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,23 @@ import * as React from "react"
 
 const Contact = () => {
     return(
-        <h1>コンタクトページ</h1>
+        <>
+            <div>
+                <div>
+                    <h1>コンタクトページ</h1>
+                    <p>お気軽にご連絡ください</p>
+                    <form>
+                        <label htmlFor="name">お名前</label>
+                        <input type="text" name="name" id="name" required />
+                        <label htmlFor="email">メールアドレス</label>
+                        <input type="email" name="email" id="email" required />
+                        <label htmlFor="textarea"> ご用件</label>
+                        <textarea  name="message" rows="10" id="textarea" required></textarea>
+                        <button type="submit">送信</button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
