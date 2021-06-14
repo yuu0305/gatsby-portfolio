d8b79ae2afa79de6529b2fe46b62957a4f76da16
yuu0305 <segyayuu8@gmail.com>
2021-06-14 22:41:10 +0900

    Add 404 page
    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import * as Layout  from "../components/layout"
+
+const NotFoundPage = () => (
+    <Layout>
+        <div style={{textAlign: "center", height: "70vh"}}>
+            <h1>404:NotFoundPage</h1>
+            <p>ページが見つかりません</p>
+        </div>
+    </Layout>
+)
+
+export default NotFoundPage
